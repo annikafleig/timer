03ab17f5bc56aff29ceea6defe0bff9504ea9e0e
Annika Fleig <96888274+annikafleig@users.noreply.github.com>
2025-01-17 22:47:18 +0100

    add main elements to web page, implement light and dark mode

--- js/script.js
+++ js/script.js
@@ -1,1 +1,29 @@
 'use strict';
+
+// variables
+
+const body = document.querySelector(`body`);
+
+const btnStartTimer = document.querySelector(`.btn--start-timer`);
+const btnChangeMode = document.querySelector(`.btn--change-mode`);
+
+// timer
+
+btnStartTimer.addEventListener(`click`, function () {
+  console.log(`Start timer`);
+});
+
+// change mode (light / dark)
+
+btnChangeMode.addEventListener(`click`, function () {
+  body.classList.toggle(`body--color-lm`);
+  body.classList.toggle(`body--color-dm`);
+  btnStartTimer.classList.toggle(`button--color-lm`);
+  btnStartTimer.classList.toggle(`button--color-dm`);
+  btnChangeMode.classList.toggle(`button--color-lm`);
+  btnChangeMode.classList.toggle(`button--color-dm`);
+
+  btnChangeMode.textContent = `${
+    body.classList.contains(`body--color-lm`) ? `Dunkler` : `Heller`
+  } Modus`;
+});
