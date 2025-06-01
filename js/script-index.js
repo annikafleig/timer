3971aeb922f8b7b72ef0b6c5b7f8a246208c3336
Annika Fleig <mail@annikafleig.com>
2025-06-01 11:54:41 +0200

    refactoring (sticky footer, adjust indentation, move script tag to head)

--- js/script-index.js
+++ js/script-index.js
@@ -17,90 +17,93 @@ let timer;
 // timer
 
 const startTimer = function () {
-  // set time to user input and convert to seconds
-  let time =
-    +inputHours.value * 60 * 60 +
-    +inputMinutes.value * 60 +
-    +inputSeconds.value;
-  inputHours.value = ``;
-  inputMinutes.value = ``;
-  inputSeconds.value = ``;
+    // set time to user input and convert to seconds
+    let time =
+        +inputHours.value * 60 * 60 +
+        +inputMinutes.value * 60 +
+        +inputSeconds.value;
+    inputHours.value = ``;
+    inputMinutes.value = ``;
+    inputSeconds.value = ``;
 
-  if (time > 0) {
-    const tick = function () {
-      const hours = String(Math.trunc(time / (60 * 60))).padStart(2, 0);
-      const min = String(Math.trunc((time % (60 * 60)) / 60)).padStart(2, 0);
-      const sec = String((time % (60 * 60)) % 60).padStart(2, 0);
+    if (time > 0) {
+        const tick = function () {
+            const hours = String(Math.trunc(time / (60 * 60))).padStart(2, 0);
+            const min = String(Math.trunc((time % (60 * 60)) / 60)).padStart(
+                2,
+                0
+            );
+            const sec = String((time % (60 * 60)) % 60).padStart(2, 0);
 
-      // in each call print the remaining time to the UI
-      labelTimer.textContent = `${hours}:${min}:${sec}`;
+            // in each call print the remaining time to the UI
+            labelTimer.textContent = `${hours}:${min}:${sec}`;
 
-      // time's up (clear timer and show alter window)
-      if (time === 0) {
-        clearInterval(timer);
-        // without setTimeout() the remaining time is only displayed as 00:00:00 AFTER the alert window is closed
-        setTimeout(() => {
-          alert(`Die Zeit ist um`);
-        }, 100);
-      }
+            // time's up (clear timer and show alter window)
+            if (time === 0) {
+                clearInterval(timer);
+                // without setTimeout() the remaining time is only displayed as 00:00:00 AFTER the alert window is closed
+                setTimeout(() => {
+                    alert(`Die Zeit ist um`);
+                }, 100);
+            }
 
-      // decrease 1s
-      time--;
-    };
+            // decrease 1s
+            time--;
+        };
 
-    // call the timer functionality immediately
-    tick();
-    // call the timer every second
-    timer = setInterval(tick, 1000);
-    return timer;
-  } else {
-    alert(`Bitte eine positive Zahl eingeben`);
-    labelTimer.textContent = `00:00:00`;
-  }
+        // call the timer functionality immediately
+        tick();
+        // call the timer every second
+        timer = setInterval(tick, 1000);
+        return timer;
+    } else {
+        alert(`Bitte eine positive Zahl eingeben`);
+        labelTimer.textContent = `00:00:00`;
+    }
 };
 
 btnStartTimer.addEventListener(`click`, function () {
-  // clear existing timer if necessary
-  if (timer) clearInterval(timer);
-  timer = startTimer();
+    // clear existing timer if necessary
+    if (timer) clearInterval(timer);
+    timer = startTimer();
 });
 
 document.addEventListener(`keydown`, function (e) {
-  if (e.key === `Enter`) {
-    if (
-      inputHours.value != `` ||
-      inputMinutes.value != `` ||
-      inputSeconds.value != ``
-    ) {
-      // clear existing timer if necessary
-      if (timer) clearInterval(timer);
-      timer = startTimer();
-      inputHours.blur();
-      inputMinutes.blur();
-      inputSeconds.blur();
+    if (e.key === `Enter`) {
+        if (
+            inputHours.value != `` ||
+            inputMinutes.value != `` ||
+            inputSeconds.value != ``
+        ) {
+            // clear existing timer if necessary
+            if (timer) clearInterval(timer);
+            timer = startTimer();
+            inputHours.blur();
+            inputMinutes.blur();
+            inputSeconds.blur();
+        }
     }
-  }
 });
 
 // change mode (light / dark)
 
 btnChangeMode.addEventListener(`click`, function () {
-  body.classList.toggle(`body--color-lm`);
-  body.classList.toggle(`body--color-dm`);
-  btnStartTimer.classList.toggle(`btn--color-lm`);
-  btnStartTimer.classList.toggle(`btn--color-dm`);
-  btnChangeMode.classList.toggle(`btn--color-lm`);
-  btnChangeMode.classList.toggle(`btn--color-dm`);
+    body.classList.toggle(`body--color-lm`);
+    body.classList.toggle(`body--color-dm`);
+    btnStartTimer.classList.toggle(`btn--color-lm`);
+    btnStartTimer.classList.toggle(`btn--color-dm`);
+    btnChangeMode.classList.toggle(`btn--color-lm`);
+    btnChangeMode.classList.toggle(`btn--color-dm`);
 
-  btnChangeMode.textContent = `${
-    body.classList.contains(`body--color-lm`) ? `Dunkler` : `Heller`
-  } Modus`;
+    btnChangeMode.textContent = `${
+        body.classList.contains(`body--color-lm`) ? `Dunkler` : `Heller`
+    } Modus`;
 
-  footer.classList.toggle(`footer--color-lm`);
-  footer.classList.toggle(`footer--color-dm`);
+    footer.classList.toggle(`footer--color-lm`);
+    footer.classList.toggle(`footer--color-dm`);
 
-  AllFooterNavA.forEach((a) => {
-    a.classList.toggle(`a--color-lm`);
-    a.classList.toggle(`a--color-dm`);
-  });
+    AllFooterNavA.forEach((a) => {
+        a.classList.toggle(`a--color-lm`);
+        a.classList.toggle(`a--color-dm`);
+    });
 });
--- js/script-other.js
+++ js/script-other.js
@@ -7,21 +7,21 @@ const footerPlaceholder = document.querySelector(`.footer--placeholder`);
 // load footer (Datenschutz und Impressum)
 
 const loadFooter = function () {
-  footerPlaceholder.innerHTML = `<div>
-  <button class="btn a--color-plain btn--footer">
-    <a href="./index.html" class="footer--timer">Timer</a>
-  </button>
-</div>
-<div class="footer--nav-name">
-  <nav class="footer--nav">
-    <a href="./datenschutz.html" class="footer--nav-a a--color-plain"
-      >Datenschutz</a
-    >
-    <a href="./impressum.html" class="footer--nav-a a--color-plain"
-      >Impressum</a
-    >
-  </nav>
-  <div class="footer--name">AHF</div>
-</div>`;
+    footerPlaceholder.insertAdjacentHTML(
+        `beforeend`,
+        /*HTML*/
+        `<div>
+            <button class="btn a--color-plain btn--footer">
+                <a href="./index.html" class="footer--timer">Timer</a>
+            </button>
+        </div>
+        <div class="footer--nav-name">
+            <nav class="footer--nav">
+                <a href="./datenschutz.html" class="footer--nav-a a--color-plain">Datenschutz</a>
+                <a href="./impressum.html" class="footer--nav-a a--color-plain">Impressum</a>
+            </nav>
+            <div class="footer--name">AHF</div>
+        </div>`
+    );
 };
 loadFooter();
